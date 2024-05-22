bf529ae8bc63d13d3f65d016a8d67cbc185d3d64
mmc <admin@example.com>
2024-05-22 13:53:02 +0800

    Add Cli Tools

--- internal/logic/airdrop.go
+++ internal/logic/airdrop.go
@@ -19,8 +19,17 @@ func (l *Producer) Airdrop() {
 		if err != nil {
 			logx.Errorf("err :%v", err)
 		}
+
 		logx.Infof("signature: %v amount:%v account:%v    ", out.String(), account.PublicKey(), 100)
 		time.Sleep(1)
+	}
+
+	for _, accout := range l.svcCtx.AddrList {
+		balance, err := l.svcCtx.SolCli.GetBalance(l.ctx, accout.PublicKey(), rpc.CommitmentFinalized)
+		if err != nil {
+			return
+		}
+		logx.Infof("account :%v amount:%v    ", accout.PublicKey(), balance.Value)
 	}
 
 }
