617725668b0c662cc2efc2bde4dfad34f4293e3e
SFHJavaer <2428670182@qq.com>
2024-05-25 14:16:56 +0800

    ignore error message and fix tokensminted

--- internal/logic/balance.go
+++ internal/logic/balance.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	bin "github.com/gagliardetto/binary"
 	"math/big"
 	"solxen-tx/internal/logic/generated/sol_xen_miner"
 	"solxen-tx/internal/logic/generated/sol_xen_minter"
@@ -24,12 +25,13 @@ func (l *Producer) Balance() error {
 		}
 		balanceInSOL := float64(balance.Value) / 1_000_000_000
 
-		// 获取points,tokens
 		var (
 			userSolXnRecordPda    solana.PublicKey
 			user_balance_data_raw sol_xen_minter.UserTokensRecord
 			userSolAccountDataRaw sol_xen_miner.UserSolXnRecord
+			programId = solana.MustPublicKeyFromBase58(l.svcCtx.Config.Sol.ProgramId)
 		)
+		// 获取points，tokens
 		userSolXnRecordPda, _, err = solana.FindProgramAddress(
 			[][]byte{
 				[]byte("xn-by-sol"),
@@ -41,14 +43,28 @@ func (l *Producer) Balance() error {
 		if err != nil {
 			return errorx.Wrap(err, "global_xn_record_pda")
 		}
+		user_token_record_pda, _, err := solana.FindProgramAddress(
+			[][]byte{
+				[]byte("sol-xen-minted"),
+				account.PublicKey().Bytes(),
+			},
+			programId,
+		)
+		if err != nil {
+			return errorx.Wrap(err, "user_eth_xn_record_pda")
+		}
+
 		err = l.svcCtx.SolCli.GetAccountDataInto(
 			l.ctx,
 			userSolXnRecordPda,
 			&userSolAccountDataRaw,
 		)
+		resp, err := l.svcCtx.SolCli.GetAccountInfoWithOpts(l.ctx, user_token_record_pda, &rpc.GetAccountInfoOpts{
+			Commitment: rpc.CommitmentConfirmed,
+		})
 		if err != nil {
-			logx.Infof("failed to get account data: %v", err)
-			// return err
+		} else {
+			err = bin.NewBinDecoder(resp.Value.Data.GetBinary()).Decode(&user_balance_data_raw)
 		}
 		// 打印账户余额和 tokens 信息
 		logx.Infof("account: %v  balance: %.7f SOL  points : %v   tokens: %v",
